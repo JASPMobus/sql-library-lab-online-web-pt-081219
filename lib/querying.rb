a80c534204d1695633b43c92c03821afb6e9d7e9
James Mobus <mobusj@live.com>
2019-11-07 23:02:20 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -16,7 +16,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT series.title FROM series INNER JOIN books ON series.id = books.series_id INNER JOIN characters ON books.id INNER JOIN "
 end
 
 def select_character_names_and_number_of_books_they_are_in
